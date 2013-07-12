5f60936d4bf2a1dc3555a2356a82ceee5d30f57a
Ubuntu <ubuntu@hades.(none)>
2013-07-12 04:59:16 +0000

    edited webjs and added index

--- web.js
+++ web.js
@@ -2,14 +2,18 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-#app.get('/', function(request, response) {
-#  response.send('Hello World2!');
-#});
+//app.get('/', function(request, response) {
+//var fs = require('fs');
+//var buffer = new buffer ();
+//  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+//});
+
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
- });
+    var fs = require('fs');
+    var content = fsReadFileSync('index.html', 'utf-8');
+    response.send(content);
+});
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
